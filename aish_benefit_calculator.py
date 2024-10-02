3457439f896469688b7d90b680a1f69c7b475a16
wei.wei <monkalways@gmail.com>
2024-10-01 21:18:56 -0600

    Some change

--- aish_benefit_calculator.py
+++ aish_benefit_calculator.py
@@ -4,7 +4,7 @@ from streamlit_authenticator.utilities.hasher import Hasher
 import requests
 import yaml
 from yaml.loader import SafeLoader
-
+ 
 with open('./config.yaml') as file:
   config = yaml.load(file, Loader=SafeLoader)
     
