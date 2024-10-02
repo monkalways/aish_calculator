9d2b062f36186ad23c8f95bb1527303312792875
wei.wei <monkalways@gmail.com>
2024-10-01 21:18:07 -0600

    rollback

--- aish_benefit_calculator.py
+++ aish_benefit_calculator.py
@@ -5,19 +5,6 @@ import requests
 import yaml
 from yaml.loader import SafeLoader
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
 with open('./config.yaml') as file:
   config = yaml.load(file, Loader=SafeLoader)
     
