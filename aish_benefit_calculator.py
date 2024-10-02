2cd604f3cb48980092dceae2884a1ccfd7373841
wei.wei <monkalways@gmail.com>
2024-10-01 21:16:19 -0600

    Hide github

--- aish_benefit_calculator.py
+++ aish_benefit_calculator.py
@@ -5,13 +5,18 @@ import requests
 import yaml
 from yaml.loader import SafeLoader
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 with open('./config.yaml') as file:
   config = yaml.load(file, Loader=SafeLoader)
